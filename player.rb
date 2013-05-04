d934b01c16b5533152f84855b975e03675a6a5be
Daniel King <daniel@fake.fake>
2013-05-04 14:31:55 -0700

    player updates the frame when new_roll is called

--- player.rb
+++ player.rb
@@ -1,4 +1,8 @@
+
+
 class Player
+
+  attr_accessor :current_frame
   def score
     @current_score = 0
     frames.each do |frame| 
@@ -6,4 +10,9 @@ class Player
     end
     @current_score
   end
+  
+  def new_roll(score)
+    @current_frame.new_roll score
+  end
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,17 @@
 require_relative '../player.rb'
+require_relative '../frame.rb'
 
 describe Player do
   it 'returns a score based on total frame scores' do
     test_player = Player.new
-    test_player.stub(:frames => [stub(:score => 5), stub(:score => 10)])
+    test_player.stub(:frames).and_return([stub(:score => 5), stub(:score => 10)])
     test_player.score.should == 15
+  end
+  it 'updates the frame score after each roll' do
+    test_player = Player.new
+    test_frame = double('frame')
+    test_frame.should_receive(:new_roll).with(7)
+    test_player.current_frame = test_frame
+    test_player.new_roll(7)
   end
 end

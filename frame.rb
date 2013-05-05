7365ce2a3e5c5e84d9f35991e01abc31560cc43f
Daniel King <daniel@fake.fake>
2013-05-05 07:40:28 -0700

    frame returns next after a strike and tracks if strike_or_spare

--- frame.rb
+++ frame.rb
@@ -1,10 +1,21 @@
 class Frame
+
+  attr_reader :strike_or_spare
+
   def score
     @first_roll.to_i + @second_roll.to_i
   end
   
   def new_roll(score)
-    if @first_roll
+    if score == 10 && @first_roll.nil?
+      @first_roll = score
+      @strike_or_spare = :strike
+      :next
+    elsif @first_roll.to_i + score == 10
+      @second_roll = score
+      @strike_or_spare = :spare
+      :next
+    elsif @first_roll
       @second_roll = score
       :next
     else
--- spec/frame_spec.rb
+++ spec/frame_spec.rb
@@ -1,31 +1,50 @@
 require_relative '../frame.rb'
 
 describe Frame do
-  it 'is initialized with a score of 0' do
-    test_frame = Frame.new
-    test_frame.score.should == 0
+  before(:each) do
+    @test_frame = Frame.new
   end
-  it 'gives a score of 8 after a new rolls of 2 and 6' do
-    test_frame = Frame.new
-    test_frame.new_roll 2
-    test_frame.new_roll 6
-    test_frame.score.should == 8
+
+  it 'is initialized with a score of 0' do
+    @test_frame.score.should == 0
+  end
+  it 'gives a score of 8 after new rolls of 2 and 6' do
+    @test_frame.new_roll 2
+    @test_frame.new_roll 6
+    @test_frame.score.should == 8
   end
   it 'gives a score of 3 after a roll of 3' do
-    test_frame = Frame.new
-    test_frame.new_roll 3
-    test_frame.score.should == 3
+    @test_frame.new_roll 3
+    @test_frame.score.should == 3
   end
   it 'gives a score of 4 after new rolls of 0 and 4' do
-    test_frame = Frame.new
-    test_frame.new_roll 0
-    test_frame.new_roll 4
-    test_frame.score.should == 4
+    @test_frame.new_roll 0
+    @test_frame.new_roll 4
+    @test_frame.score.should == 4
   end
   it 'returns next after a second roll' do
-    test_frame = Frame.new
-    test_frame.new_roll 2
-    test_frame.new_roll(6).should == :next
+    @test_frame.new_roll 2
+    @test_frame.new_roll(6).should == :next
+  end
+  it 'returns next after a first roll of 10 - a strike' do
+    @test_frame.new_roll(10).should == :next
+  end
+  it 'does not return next after a first roll of 9' do
+    @test_frame.new_roll(9).should_not == :next
+  end
+  it 'sets strike_or_spare to strike after a first score of 10' do
+    @test_frame.new_roll(10)
+    @test_frame.strike_or_spare.should == :strike
+  end
+  it 'sets strike_or_spare to spare after a roll of 4 and roll of 6' do
+    @test_frame.new_roll(4)
+    @test_frame.new_roll(6)
+    @test_frame.strike_or_spare.should == :spare
+  end
+  it 'sets strike_or_spare to spare after a roll of 0 and roll of 10' do
+    @test_frame.new_roll(0)
+    @test_frame.new_roll(10)
+    @test_frame.strike_or_spare.should == :spare
   end
 
 end
